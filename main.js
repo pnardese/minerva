b9fee24c73ed13e6c824800426f4ffec90ab603b
pnardese <pnardese@gmail.com>
2023-02-27 15:25:31 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,46 @@
+import './style.css'
+const html = document.documentElement;
+const canvas = document.getElementById("scrollin-frames");
+const context = canvas.getContext("2d");
+
+const frameCount = 200;
+const currentFrame = index => (
+  `/frames_anim/${index.toString().padStart(4, '0')}.jpg`
+)
+
+const preloadImages = () => {
+  for (let i = 1; i < frameCount; i++) {
+    const img = new Image();
+    img.src = currentFrame(i);
+  }
+};
+
+const img = new Image()
+img.src = currentFrame(1);
+canvas.width=1280;
+canvas.height=700;
+
+// canvas.width=1158;
+// canvas.height=770;
+img.onload=function(){
+  context.drawImage(img, 0, 0);
+}
+
+const updateImage = index => {
+  img.src = currentFrame(index);
+  context.drawImage(img, 0, 0);
+}
+
+window.addEventListener('scroll', () => {  
+  const scrollTop = html.scrollTop;
+  const maxScrollTop = html.scrollHeight - window.innerHeight;
+  const scrollFraction = scrollTop / maxScrollTop;
+  const frameIndex = Math.min(
+    frameCount - 1,
+    Math.ceil(scrollFraction * frameCount)
+  );
+  
+  requestAnimationFrame(() => updateImage(frameIndex + 1))
+});
+
+preloadImages()
